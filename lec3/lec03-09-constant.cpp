e8eed1458ad5214339b793c12d657a3c54f8edee
shuminghao <shuminghao677@gmail.com>
2026-01-28 11:18:12 +0800

    add:关于数组的输入输出遍历 写死变量以及求和

--- lec3/lec03-09-constant.cpp
+++ lec3/lec03-09-constant.cpp
@@ -4,12 +4,23 @@ using namespace std;
 int main() {
 
 	int n = 10;
-
-	int mark[n];
-
-
+	//#define SIZE 10; 把变量写死
+	int mark[SIZE];
+	for (int i=0;i<SIZE;i++){
+		cin>>mark[i];
+	}
+    
 	// TODO: input for all elements
+	int sum=0;
+	cout<<"[";
+    for (int j=0;j<SIZE;j++){
+		cout<<mark[j]<<"  ";
+		sum+=mark[j];
 
-
+	}
+    cout<<sum<<endl;
+	cout<<"]";
 	return 0;
 }
+//输出部分也要用循环 直接 mark就是指针 然后mark[n]里面是数组里面的第几个元素
+
