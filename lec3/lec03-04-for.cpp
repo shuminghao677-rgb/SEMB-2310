7f6064dbd01999c3edecb95813c2a38ac2b2693a
shuminghao <shuminghao677@gmail.com>
2026-02-01 12:11:22 +0800

    add:加上了条件还有vector创建数组

--- lec3/lec03-04-for.cpp
+++ lec3/lec03-04-for.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 using namespace std;
+ 
+// 一个关于while 输入的计算sum值的函数
+// int main() {
 
-int main() {
+// 	int sum = 0;
+// 	int num;
+// 	while (cin>>num && num!=-1){
+// 		sum+=num;
+// 	}
+// 	cout << "sum is: " << sum << endl;
 
-	int sum = 0;
+// 	return 0;
+// }
 
+//用创建数组 for 循环实现
+int main(){
+    vector<int>arr1;
+    int num;
 
-	cout << "sum is: " << sum << endl;
+    while(cin>>num && num!=-1){
+        arr1.push_back(num);
+    }
 
-	return 0;
+    int sum=0;
+    for (int i=0;i<arr1.size();i++){
+        sum+=arr1[i];
+    }
+    cout<<"最后的数组是：  "<<endl;
+    for (int j=0;j<arr1.size();j++){
+        cout<<arr1[j]<<" ";
+    }
+    cout<<",和是"<<sum<<endl;
+    return 0;
 }
+
+
--- lec3/lec03-05-condition.cpp
+++ lec3/lec03-05-condition.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main() {
 	
-	int k = 5;
+	int k;
+	cout<<"please enter the integer k"<<endl;
+	cin>>k;
 
-    if(2 < k < 7) {
-		cout << "true" << endl;
+    if (k>2 && k<7) {
+		//必须用 && 不能直接  2<k<7
+		cout << "true, the k is with the range 2-7" << endl;
 	} else {
-		cout << "false" << endl;
+		cout << "false, the k is not in the range 2-7" << endl;
 	}
 	
 	return 0;
